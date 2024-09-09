2227a0130026e0180bb33ec803994205918f2a05
Hasitha <hasithapeiris.me@gmail.com>
2024-09-09 20:41:01 +0530

    feat: create clerk authentication

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,7 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
-
 export default function Home() {
   return (
     <main>
-      <Button className="px-10 py-5">Hello</Button>
+      <h1>Hello</h1>
     </main>
   );
 }
