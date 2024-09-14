a0075dc359f67eb21f77335fdd4e96e97f5bd375
Hasitha Peiris <hasithapeiris.me@gmail.com>
2024-09-14 09:00:20 +0530

    fix: fix userId error

--- app/(root)/events/create/page.tsx
+++ app/(root)/events/create/page.tsx
@@ -5,7 +5,7 @@ import React from "react";
 const CreateEvent = () => {
   const { sessionClaims } = auth();
 
-  const userId = (sessionClaims?.userId as { userId: string })?.userId;
+  const userId = sessionClaims?.userId as string;
 
   return (
     <>
