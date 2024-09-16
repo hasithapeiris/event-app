f8f77f9ba597c21a3b3d6b9068ba657ed150e218
Hasitha <hasithapeiris.me@gmail.com>
2024-09-17 00:38:49 +0530

    fix: fix checkout error

--- components/shared/Checkout.tsx
+++ components/shared/Checkout.tsx
@@ -3,6 +3,7 @@ import React, { useEffect } from "react";
 import { IEvent } from "@/lib/database/models/event.model";
 import { Button } from "../ui/button";
 import { loadStripe } from "@stripe/stripe-js";
+import { checkoutOrder } from "@/lib/actions/order.actions";
 
 loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!);
 
@@ -30,7 +31,7 @@ const Checkout = ({ event, userId }: { event: IEvent; userId: string }) => {
       buyerId: userId,
     };
 
-    // await checkoutOrder(order);
+    await checkoutOrder(order);
   };
 
   return (
